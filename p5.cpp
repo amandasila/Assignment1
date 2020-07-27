da5235f9a9158b9ff0f95d428403749c894bd576
amandasila <68819390+amandasila@users.noreply.github.com>
2020-07-27 10:40:06 +0530

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,28 @@
+//WAP to check whether the string is palindrome or not.
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+
+    char str[20];
+    int i,n=0,flag=0;
+    cout << "Input string: ";
+    cin >> str;
+    n=strlen(str);
+    for(i=0;i<n;i++)
+    {
+        if(str[i]!=str[n-i-1])
+            flag=1;
+        break;
+    }
+    if(flag==1)
+    {
+        cout << "The string is not a Palindrome\n";
+    }
+    else
+    {
+       cout << "The string is Palindrome";
+    }
+    return 0;
+}
